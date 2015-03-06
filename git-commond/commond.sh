3daccbca775174acfdb4f9c1af94dfe8543f7ed1
qlans <zhaofukaimail@gmail.com>
2015-03-06 16:18:20 +0800

    add git-commond-1
    Signed-off-by: qlans <zhaofukaimail@gmail.com>

---
+++ git-commond/commond.sh
@@ -0,0 +1,19 @@
+#Create a new ssh key, using the provided email as a label
+ssh-keygen -t rsa -C "your_email@example.com"
+
+#Attempts to ssh to GitHub
+ssh -T git@github.com
+
+https://help.github.com/articles/generating-ssh-keys/#platform-linux
+
+#===================================================================
+
+
+
+
+
+
+
+
+
+
